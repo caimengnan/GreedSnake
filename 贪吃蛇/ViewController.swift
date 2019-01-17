d9ba508fd9095c0e1ac3767610d751e434f796ad
caimengnan <caimengnan@ccclubs.com>
2019-01-17 09:41:58 +0800

    Initial Commit

---
+++ 贪吃蛇/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  贪吃蛇
+//
+//  Created by targeter on 2019/1/17.
+//  Copyright © 2019 targeter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
